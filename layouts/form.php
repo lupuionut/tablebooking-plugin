75f47d2dc84c484a2618a7f831ae914720270247
Ionut Lupu <lupu.ionut@protonmail.ch>
2022-12-10 12:22:45 +0200

    working on the restaurant search form

--- layouts/form.php
+++ layouts/form.php
@@ -1,10 +1,13 @@
 <?php
 defined('_JEXEC') or die;
 $key = $displayData['key'];
+$restaurant_id = $displayData['id'];
 ?>
 
 <div id="tb-bookingForm<?php echo $key;?>" class="bookingForm">
-    <tb-search-form v-if="idx == 0"></tb-search-form>
+    <tb-search-form
+        v-if="idx == 0"
+        restaurant="<?php echo $restaurant_id;?>"></tb-search-form>
 </div>
 
 <script type="text/javascript">
@@ -13,11 +16,39 @@ $key = $displayData['key'];
 const TbSearchForm = {
     data() {
         return {
-
+            restaurant_id: this.restaurant,
+            restaurants: []
         }
     },
+    methods: {
+        submit() {
+            console.log(this.restaurant);
+        },
+        populateRestaurant() {
+            if (this.restaurants.length != 0) {
+                return;
+            }
+            this.restaurants = [{id: 1, name: "coco"}, {id: 2, name: "jambo"}];
+        },
+        setRestaurant(id) {
+            this.restaurant_id = id;
+        },
+    },
+    props: ["restaurant"],
     template: `
         <div class="form-group">
+
+            <select v-if="this.restaurant == 0"
+                @click="populateRestaurant()">
+                <option value="0" @click="setRestaurant(0)">
+                    <?php echo JText::_('PLG_CONTENT_TABLEBOOKING_SELECT_RESTAURANT', true);?>
+                </option>
+                <option v-for="restaurant in this.restaurants"
+                    :value="restaurant.id"
+                    :key="restaurant.id"
+                    @click="setRestaurant(restaurant.id)">{{ restaurant.name }}</option>
+            </select>
+
             <input type="text"
                 name="tbcalendar"
                 id="tbcalendar" />
@@ -30,8 +61,8 @@ const TbSearchForm = {
                 name="tbto"
                 id="tbto" />
 
-            <button type="button">
-                <?php echo JText::_('PLG_CONTENT_TABLEBOOKING_SEARCH');?></button>
+            <button type="button" @click="submit">
+                <?php echo JText::_('PLG_CONTENT_TABLEBOOKING_SEARCH', true);?></button>
         </div>
     `
 };
@@ -41,7 +72,6 @@ Vue.createApp({
     data() {
         return {
             idx: 0,
-            message: "",
         }
     },
     components: {
