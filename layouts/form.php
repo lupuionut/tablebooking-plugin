fb489034e61b54ec4e5036921fe705adac3dc429
Ionut Lupu <lupu.ionut@protonmail.ch>
2022-12-10 14:20:10 +0200

    populate restaurants select if no restaurant is provided in the plugin code

--- layouts/form.php
+++ layouts/form.php
@@ -1,5 +1,10 @@
 <?php
+
+use Joomla\CMS\Session\Session;
+use Joomla\CMS\Uri\Uri;
+
 defined('_JEXEC') or die;
+
 $key = $displayData['key'];
 $restaurant_id = $displayData['id'];
 ?>
@@ -8,45 +13,46 @@ $restaurant_id = $displayData['id'];
     <tb-search-form
         v-if="idx == 0"
         restaurant="<?php echo $restaurant_id;?>"></tb-search-form>
+
 </div>
 
 <script type="text/javascript">
-
 <?php if ($displayData['isNew']) {?>
+const JoomlaToken = '<?php echo Session::getFormToken();?>';
+const JoomlaUri = '<?php echo Uri::root();?>';
 const TbSearchForm = {
     data() {
         return {
             restaurant_id: this.restaurant,
-            restaurants: []
+            form: {
+                restaurants: []
+            }
         }
     },
     methods: {
         submit() {
-            console.log(this.restaurant);
-        },
-        populateRestaurant() {
-            if (this.restaurants.length != 0) {
-                return;
-            }
-            this.restaurants = [{id: 1, name: "coco"}, {id: 2, name: "jambo"}];
-        },
-        setRestaurant(id) {
-            this.restaurant_id = id;
-        },
+            console.log(this.restaurant_id);
+        }
+    },
+    mounted() {
+        if (this.restaurant_id == 0) {
+            fetch(JoomlaUri + 'index.php?option=com_tablebooking&task=search.getRestaurants&' + JoomlaToken + '=1')
+            .then(r => r.json())
+            .then(r => { this.form.restaurants = r })
+        }
     },
     props: ["restaurant"],
     template: `
         <div class="form-group">
 
             <select v-if="this.restaurant == 0"
-                @click="populateRestaurant()">
-                <option value="0" @click="setRestaurant(0)">
+                v-model="this.restaurant_id">
+                <option value="0">
                     <?php echo JText::_('PLG_CONTENT_TABLEBOOKING_SELECT_RESTAURANT', true);?>
                 </option>
-                <option v-for="restaurant in this.restaurants"
-                    :value="restaurant.id"
-                    :key="restaurant.id"
-                    @click="setRestaurant(restaurant.id)">{{ restaurant.name }}</option>
+                <option v-for="restaurant in this.form.restaurants"
+                    :value="restaurant.value"
+                    :key="restaurant.value">{{ restaurant.name }}</option>
             </select>
 
             <input type="text"
