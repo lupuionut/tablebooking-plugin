5418ad0a9f2d5b934e43ec8950ac0ad4a2a41799
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-01-07 16:45:44 +0200

    move js/css to local assets folder

--- tablebooking.php
+++ tablebooking.php
@@ -80,10 +80,10 @@ class PlgContentTablebooking extends JPlugin
 
     protected function generateForm($key, $id) {
 
-        Factory::getDocument()->addScript('https://unpkg.com/vue@3/dist/vue.global.prod.js');
-        Factory::getDocument()->addScript('https://unpkg.com/@vuepic/vue-datepicker@latest');
-        Factory::getDocument()->addStyleSheet('https://unpkg.com/@vuepic/vue-datepicker@latest/dist/main.css');
-        Factory::getDocument()->addStyleSheet(Uri::root() . 'plugins/content/tablebooking/tablebooking.css?t=' . time());
+        Factory::getDocument()->addScript(Uri::root() . 'plugins/content/tablebooking/assets/vue.global.prod.js');
+        Factory::getDocument()->addScript(Uri::root() . 'plugins/content/tablebooking/assets/vue-datepicker@latest');
+        Factory::getDocument()->addStyleSheet(Uri::root() . 'plugins/content/tablebooking/assets/main.css');
+        Factory::getDocument()->addStyleSheet(Uri::root() . 'plugins/content/tablebooking/assets/tablebooking.css?t=' . time());
         $isNew = self::$isNewInstance;
         self::$isNewInstance = false;
         $path = PluginHelper::getLayoutPath('content', 'tablebooking', 'form');
